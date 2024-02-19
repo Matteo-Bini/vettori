fe207bc4fc924789571a47544c6a59bf66da19ab
matte <matte@DESKTOP-NA3JRQD>
2024-02-19 22:36:17 +0100

    Aggiungere i file di progetto.

---
+++ vettori/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vettori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         Vector v1 = new Vector(1, 3);
+            Vector v2 = new Vector(3, 7);
+            Console.WriteLine(v1 + v2);
+            Console.ReadLine();
+
+        }
+    }
+}
